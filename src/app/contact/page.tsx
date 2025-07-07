b94020576213ff205d45f1d4f65599d12460595a
bishwajit93 <bish.karm123@gmail.com>
2025-07-08 00:05:15 +0200

    basic folder and file structure setup

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,5 @@
+export default function Contact() {
+    return(
+        <p>Hello from contact page</p>
+    )
+}
---
+++ src/app/projects/page.tsx
@@ -0,0 +1,5 @@
+export default function Projects() {
+    return(
+        <p>Hello from Project page</p>
+    )
+}
