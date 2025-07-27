34effd1d9afb2fc2661d0f809e8293f88202acee
bishwajit93 <bish.karm123@gmail.com>
2025-07-28 00:11:47 +0200

    footer position fix bug for mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,10 @@
-// src/app/Header.tsx
-
+// src/app/layout.tsx
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import MobileNav from "@/components/MobileNav";
 import { Metadata } from "next";
 import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
-import MobileNav from "@/components/MobileNav";
 
 export const metadata: Metadata = {
   title: "Bishwajit Karmaker Portfolio",
@@ -17,14 +16,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" data-theme="dark">
-      <body className="flex flex-col min-h-screen bg-black text-white">
+    <html lang="en" data-theme="dark" className="h-full">
+      <body className="flex flex-col min-h-screen bg-black text-white overflow-x-hidden">
         <AuthProvider>
           <div className="background" />
           <Header />
           <main className="flex-1 w-full max-w-5xl mx-auto px-6 md:px-8 pt-[60px] pb-[70px] md:pt-20 md:pb-10">
-
-
             {children}
           </main>
           <Footer />
